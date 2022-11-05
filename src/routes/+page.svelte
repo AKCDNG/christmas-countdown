26cabcc16a2cc494be87d7c0581d056305dc061f
Anish <anishkakaiya@outlook.com>
2022-11-05 21:36:01 +0000

    add some bits

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Timer from '../components/Timer.svelte';
+</script>
+
+<h1>Christmas Countdown</h1>
+<Timer />
+
+<style lang="scss">
+</style>
